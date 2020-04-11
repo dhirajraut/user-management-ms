bc62ebac730afb0f0f59e477150df0f20172414e
Dhiraj Raut <Dhiraj.Raut@nice.com>
2020-04-11 20:34:09 +0530

    Default Base64

--- impl/src/main/java/com/galaxy/spring/config/ApplicationConfig.java
+++ impl/src/main/java/com/galaxy/spring/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package com.galaxy.spring;
+package com.galaxy.spring.config;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
---
+++ impl/src/main/java/com/galaxy/spring/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.galaxy.spring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure (HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+		.authorizeRequests().anyRequest().authenticated()
+		.and()
+		.httpBasic();
+	}
+	
+	@Autowired
+	protected void configureGlobal (AuthenticationManagerBuilder authenticationManager) throws Exception {
+		authenticationManager.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("USER");
+	}
+}
